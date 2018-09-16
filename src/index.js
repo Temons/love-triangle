38dd88013e4b8ad94ac410212f4f72d97e120162
Artem <algatecby@gmail.com>
2018-09-16 16:49:53 +0300

    withCycle

--- src/index.js
+++ src/index.js
@@ -4,4 +4,25 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
-};
+  let count = 0;
+  let first;
+  let second;
+  let third;
+    
+    for (let i=0; i < preferences.length; i++){
+    if(preferences[i] !== i+1){
+    first = preferences[i];
+    second = preferences[first-1];
+    third = preferences [second-1];
+    if(i == third - 1){
+      count++;
+      preferences[i] = 0;
+      preferences[(first-1)] = 0;
+      preferences[(second-1)] = 0;
+      
+   }
+  }
+}
+   
+   return count;
+  }
